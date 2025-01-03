88a28a39d0a9cbcada64d2127bf981092c0b61aa
divyaanshupal <divyanshuwork03@gmail.com>
2025-01-04 01:33:31 +0530

    testing location issue

---
+++ android/app/src/main/kotlin/com/example/weather_testing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weather_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/data.dart
@@ -0,0 +1,1 @@
+String API_KEY="93422a5dfdd6f640b32b923326f18827";
